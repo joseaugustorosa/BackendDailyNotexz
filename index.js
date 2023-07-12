afa71f2bc3769079d5f384b8c372c2de032a5b55
José Augusto Nascimento Rosa Santos <101712645+joseaugustorosa@users.noreply.github.com>
2023-07-12 08:11:57 -0300

    ok

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/login",(req,res)=>{
     [email, senha]).then((result) => {
         console.log(result.rows)
         console.log(result.rows.nome)
-        res.send({msg: "Login Autorizado", nome: result.rows[0].nome,user: result.rows[0].nome_usuario })
+        res.send({msg: "Login Autorizado", nome: result.rows[0].nome ,user: result.rows[0].nome_usuario })
       })
       .catch((error) => {
         console.error('Erro na query:', error);
@@ -65,6 +65,83 @@ app.post("/login",(req,res)=>{
             
         }
         );
+        app.post("/popularTabelaDesenvo",(req,res)=>{
+       
+          const nome = req.body.nomeuser;
+          
+          client.query("SELECT * FROM TAREFAS1 WHERE usuario= ($1)",
+          [nome]).then((result) => {
+              console.log(result.rows)
+              console.log(result.rows.titulo)
+              res.send(result)
+            })
+            .catch((error) => {
+              console.error('Erro na query:', error);
+              res.status(500).send('Erro na execução da query');
+            });
+              
+          }
+          );
+          app.post("/ExcluirBacklog",(req,res)=>{
+       
+          const titulo = req.body.titulo;
+          const data = req.body.data;
+          const hora = req.body.hora;
+          const user = req.body.user;
+          
+          client.query("DELETE FROM TAREFAS WHERE USUARIO = ($1) and titulo =($2) and hora = ($3) and data =($4)",
+          [user,titulo,hora,data]).then((result) => {
+              console.log("DELETE FROM TAREFAS WHERE USUARIO =  and titulo =($2) and hora = ($3) and data =($4)",user,titulo,hora,data)
+              console.log(result.rows)
+              console.log(result.rows.titulo)
+              res.send(result)
+            })
+            .catch((error) => {
+              console.error('Erro na query:', error);
+              res.status(500).send('Erro na execução da query');
+            });
+              
+          }
+          );
+          app.post("/popularTabelaFinished",(req,res)=>{
+       
+            const nome = req.body.nomeuser;
+            
+            client.query("SELECT * FROM TAREFAS2 WHERE usuario= ($1)",
+            [nome]).then((result) => {
+                console.log(result.rows)
+                console.log(result.rows.titulo)
+                res.send(result)
+              })
+              .catch((error) => {
+                console.error('Erro na query:', error);
+                res.status(500).send('Erro na execução da query');
+              });
+                
+            }
+            );
+app.post("/inserirNaTabelaBacklog",(req,res)=>{
+       
+          const titulo = req.body.titul;
+          const descricao = req.body.descricao;
+          const data = req.body.data;
+          const hora = req.body.hora;
+          const user = req.body.user;
+          
+          
+          client.query("insert into tarefas(titulo, descricao, data, hora, usuario)  values (($1),($2),($3),($4),($5))",
+          [titulo,descricao,data,hora,user]).then((result) => {
+              console.log(result.rows)
+              console.log(result.rows.titulo)
+              res.send(result)
+            })
+            .catch((error) => {
+              console.error('Erro na query:', error);
+              res.status(500).send('Erro na execução da query');
+            });
+              
+          }
+          );
 
 app.listen(3001, ()=>{
     console.log("rodando")
