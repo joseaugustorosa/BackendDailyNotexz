43c2a0a024e6b7d6707394c7d427d7745393ce9d
José Augusto Nascimento Rosa <augusto.rosarj@gmail.com>
2023-08-10 21:42:35 -0300

    alteração backend sidebar

--- chamadas/quadros.js
+++ chamadas/quadros.js
@@ -24,17 +24,21 @@ async function buscarQuadros(req, res) {
 }
 async function CriarQuadros(req, res) {
     const user = req.body.nomeuser;
-    const nome = req.body.title;
+    const nome = req.body.nameQuadro;
+    console.log(user)
+    console.log(nome)
+
    
-    if (user === null || user.trim() === '' ||user === null || user.trim() === '' ) {
+    if (user === null || user.trim() === '' ||nome === null || nome.trim() === '' ) {
         res.send({msg: "DADO INDEFINIDO"})
     } else {
-        client.query("insert into quadros(usuario, quadro) values ('($1)','($2)')",
+        client.query("insert into quadros(usuario, quadro) values (($1),($2))",
         [user,nome]).then((result) => {
             res.send(result)
         })
         .catch((error) => {
-            res.send({msg: "NÃO ENCONTRADO"})
+            res.send(error)
+            console.log(error)
         });    
     }        
 }
