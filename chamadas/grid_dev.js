5981e0303414332b58624283b4dbb04a3e8119fa
José Augusto Nascimento Rosa <augusto.rosarj@gmail.com>
2024-02-21 22:54:45 -0300

    ainda

--- chamadas/grid_dev.js
+++ chamadas/grid_dev.js
@@ -17,4 +17,26 @@ async function BuscarDevInfo(req,res) {
             res.status(500).send('Erro na execução da query');
     });          
 }
-module.exports = { BuscarDevInfo};
+async function InsertDevInfo(req,res) {
+  
+    const user = req.body.user;  
+    const descricao = req.body.descricao;  
+    const tempo = req.body.tempo;  
+    const data_inicio = req.body.data_inicio;  
+    const data_fim = req.body.data_fim;  
+    const extra_feedback = req.body.extra_feedback;  
+    const responsavel = req.body.responsavel;  
+    const setor = req.body.setor;  
+    const nome = req.body.nome;  
+    const otimizacao = req.body.otimizacao;  
+     
+    client.query("INSERT INTO DEV (nome, descricao, tempo, data_inicio,data_fim,extra_feedback,responsavel,setor,otimizacao,usuario) values (($1), ($2),($3),($4),($5),($6),($7),($8),($9),($10))",
+    [nome, descricao, tempo,data_inicio, data_fim, extra_feedback, responsavel, setor,otimizacao,user]).then((result) => {       
+        res.send(result)
+    })
+    .catch((error) => {
+            console.error('Erro na query:', error);
+            res.status(500).send('Erro na execução da query');
+    });          
+}
+module.exports = { BuscarDevInfo,InsertDevInfo};
